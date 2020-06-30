55814379bedc62369b0f520e4d6e0dca87143b6e
Conny <constanze.becker@t-online.de>
2020-06-30 22:22:05 +0200

    fixed first bugs for signup and login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import NavBar from "./components/NavBar";
 import Welcome from "./components/Welcome";
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {register} from './UserFunction'
-import {Input} from 'reactstrap'
+import {Input, Form, FormGroup} from 'reactstrap'
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 class Register extends Component {
     constructor() {
@@ -33,14 +35,14 @@ class Register extends Component {
 
         register(newUser).then(res => {
             if(this.state.password == this.state.confirmpassword) {
-                this.props.histiry.push('/login')
+                this.props.history.push('/login')
             }
-            console.log(err)
+
         })
     }
 
     render() {
-        retrun (
+        return (
             <div className="container">
                 <h1 className="h3 mb-3 font-weight-normal">Sign up!</h1>
                 <form noValidate onSubmit={this.onSubmit}>
@@ -71,18 +73,29 @@ class Register extends Component {
                     <div className="row">
                         <div className="col-md-6 left">
                             <div className="form-group">
-                                <label htmlFor="studyprogram">Study program</label>
-                                <input type="select"
-                                       className="form-control"
-                                       name="studyprogram"
-                                       placeholder="Study program"
-                                       value={this.state.studyprogram}
-                                       onChange={this.onChange}>
-                                    <option>Master Angewandte Informatik</option>
-                                    <option>MasterComputer Engineering</option>
-                                    <option>Master Angewandte Kognitions- und Medienwissenschaft</option>
-                                    <option><input type="text" className="form-control" placeholder="Other" /></option>
-                                </input>
+
+                                <Form>
+                                    <FormGroup>
+                                        <label htmlFor="studyprogram">Study program</label>
+                                        <Input type="select" name="studyprogram" >
+                                            <option>Master Angewandte Informatik</option>
+                                            <option>MasterComputer Engineering</option>
+                                            <option>Master Angewandte Kognitions- und Medienwissenschaft</option>
+                                        </Input>
+
+                                    </FormGroup>
+                                </Form>
+                                {/*<input type="select"*/}
+                                {/*       className="form-control"*/}
+                                {/*       name="studyprogram"*/}
+                                {/*       placeholder="Study program"*/}
+                                {/*       value={this.state.studyprogram}*/}
+                                {/*       onChange={this.onChange}>*/}
+                                {/*    <option>Master Angewandte Informatik</option>*/}
+                                {/*    <option>MasterComputer Engineering</option>*/}
+                                {/*    <option>Master Angewandte Kognitions- und Medienwissenschaft</option>*/}
+                                {/*    <option><input type="text" className="form-control" placeholder="Other" /></option>*/}
+                                {/*</input>*/}
                             </div>
                         </div>
                         <div className="col-md-6 right">
