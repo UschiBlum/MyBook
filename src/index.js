8c4a31a5552b837eb7b4259a06a94b59153cdd9f
Jule Zirger <julezirger@stud.uni-due.de>
2020-07-01 15:28:37 +0200

    created side Templates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap-4.3.1-dist/css/bootstrap.min.css'
+import $ from 'jquery';
+import Popper from 'popper.js';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
