e4d7771b5827d1ba41fd59f05cb9ca5921d6c350
Jule Zirger <julezirger@stud.uni-due.de>
2020-07-01 16:20:31 +0200

    changed WelcomePage, added Video and 'logo'

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react'
+import ReactPlayer from "react-player"
+import logo from './logo.jpg';
+
+const divStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '20px',
+  };
+  
 
 class Welcome extends Component {
     render() {
         return (
+            <div>
             <div className="container">
                 <div className="jumbotron mt-5">
                     <div className="col-sm-8 mx-auto">
@@ -10,6 +21,18 @@ class Welcome extends Component {
                     </div>
                 </div>
             </div>
+            <div style={divStyle}>
+                <div className="logo">
+                    <img src={logo} width="500" height="500" />
+                </div>
+                <div>
+                    <ReactPlayer
+                    url="https://www.youtube.com/watch?v=zkVFHDtQ3Bk&t"
+                    />
+                </div>
+            </div>
+            </div>
+            
         )
     }
 }
