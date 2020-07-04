c672edb75b5444c5a1f395b962d16583e448ee34
Conny <constanze.becker@t-online.de>
2020-07-04 19:39:58 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
         <Router>
-          <div className="App" style={{backgroundImage: `url(${notebook})`, backgroundSize: 'cover'}}>
+          <div className="App" style={{backgroundImage: `url(${notebook})`, backgroundSize: 'cover', height:'100vh'}}>
             <NavBar />
             <Route exact path="/" component={Welcome} />
             <div className="container">
@@ -31,6 +31,22 @@ class App extends Component {
               <Route path="/notes" component ={Notes}/>
             </div>
           </div>
+          <footer>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-auto">
+                  <a href="#"></a>
+                  <a className="mx-3" href="#"></a>
+                  <a href="#"></a>
+                </div>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-auto">
+                  <p className="copyright text-muted">Copyright &copy; Donotreact 2020</p>
+                </div>
+              </div>
+            </div>
+          </footer>
         </Router>
     );
   }
