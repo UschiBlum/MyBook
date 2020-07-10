8d89e760b1264273a951dd89a3159f07501c84cd
Conny <constanze.becker@t-online.de>
2020-07-10 14:49:30 +0200

    footer and background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,30 @@
 import React, {Component} from 'react'
+import './navbar.css'
 
 class Footer extends Component {
     render() {
         return (
             <footer>
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-auto">
-                            <a href="#"></a>
-                            <a className="mx-3" href="#"></a>
-                            <a href="#"></a>
+                    <hr />
+                    <div className="row justify-content-center" style={{height: '3em'}}>
+                        <div className="col-md-4 footer-brand animated fadeInLeft" >
+                           <p className="copyright text-muted text-center text-footer">Copyright &copy; Donotreact 2020</p>
+                        </div>
+                        <div className="col-md-3 footer-nav animated fadeInUp">
+                            <a href="https://github.com/" className="text-footer">GitHub</a>
+                        </div>
+                        <div className="col-md-3 footer-nav animated fadeInUp">
+                            <a href="https://youtube.com/" className="text-footer">YouTube</a>
+                        </div>
+                        <div className="col-md-2 footer-nav animated fadeInUp">
+                            <a href="https://heroku.com/" className="text-footer">HEROKU</a>
                         </div>
                     </div>
                     <div className="row justify-content-center">
-                        <div className="col-auto">
-                            <p className="copyright text-muted">Copyright &copy; Donotreact 2020</p>
+                        <div className="col-auto" style={{height: '3em'}}>
+                            <h3>With MyBook to your book!</h3>
                         </div>
                     </div>
-                </div>
             </footer>
         )
     }
