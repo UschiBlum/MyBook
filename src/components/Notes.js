e794686c60a05a626268bf8c4aaac2e9fe7d40c8
Jule Zirger <jule.zirger@stud.uni-due.de>
2020-07-11 17:02:02 +0200

    Changed Notes Side

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Form, TextArea } from 'semantic-ui-react'
+import jwt_decode from 'jwt-decode'
 
 
 const divStyle = {
@@ -22,41 +23,110 @@ const divStyle = {
 
   const notestyle = {
     color: "white",
+    width: "200px",
     backgroundColor: colors[Math.floor(Math.random() * (max - min)) + min],
-    padding: "60px",
+    alignItems: 'center',
+    padding: "50px",
+    justifyContent: 'center',
     fontFamily: 'Arial'
   };
 
   
+  
 
 class Notes extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            notes: ''
+        }
+    }
+
+    componentDidMount () {
+        const token = localStorage.usertoken
+        const decoded = jwt_decode(token)
+        this.setState({
+            username: decoded.identity.username,
+            notes: decoded.identity.notes,
+        })
+        
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        const user = {
+            username: this.state.username,
+            notes: this.state.notes
+        }
+
+        Notes(user)
+            .then(res => {
+                if (!res.error) {
+                    const token = localStorage.usertoken
+                    const decoded = jwt_decode(token)
+                    this.setState({
+                        notes: decoded.identity.notes + this.state.TextArea,
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
     render() {
         return (
             <div className="container">
-                <div className="mt-5">
-                    <div className="col-sm-8 mx-auto">
-                        <h1 className="text-center">Notes</h1>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row first-row"></div>
                     </div>
                 </div>
-                <div style={divStyle}>
-                    <div style={divStyle}>
-                        <div style={secondStyle}>
-                            <h2 style={notestyle}>Hier ist ein note</h2>
-                        </div>
-                        <div style={secondStyle}>
-                            <h2 style={notestyle}>Hier ist ein note</h2>
-                        </div>
-                        <div style={secondStyle}>
-                            <h2 style={notestyle}>Hier ist ein note</h2>
-                        </div>
-                    </div>
-                    <div>
-                        <Form>
-                            <TextArea placeholder='Write a Note' style={{ minHeight: 300 , minWidth: 500}} />
-                        </Form>
+                <div className="row header-row">
+                    <div className="col-md-5">
+                        <h1 className="heading text-center display-1">Notes</h1>
                     </div>
                 </div>
+                <div className="col-md-5 left">
+                    <div className="form-group">
+                        <div >
+                            <div className="col-md-5 left">
+                                <h2 style = {notestyle}>Hier ist ein note 1</h2>
+                            </div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-5">
+                                <h2 style = {notestyle}>Hier ist ein note 2</h2>
+                            </div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-5">
+                                <h2 style = {notestyle}>Hier ist ein note 3</h2>
+                            </div>
+                            <div className="col-md-2"></div>
+                        </div>
+                    </div>
+                </div>
+                    <div className="col-md-2"></div>
+                    <div className="col-md-5 right">
+                        <div className = "form-group">
+                            <Form>
+                                <TextArea placeholder='Write a Note' style={{ minHeight: 300 , minWidth: 500}} />
+                            </Form>
+                            <input type="text"
+                                       className="form-control form-control-lg"
+                                       name="text"
+                                       value={this.state.TextArea}
+                                       onChange={this.handleChange}
+                                       noValidate
+                                />
+                        </div>
+                        <div>
+                            <button type="submit" className="btn btn-primary btn-lg">
+                                 Create Note
+                            </button>
+                        </div>
+                    </div>
             </div>
         )
     }
